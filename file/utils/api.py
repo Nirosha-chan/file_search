7bdb779ad8412f284595dc2b2280c38fb46ae29b
Nirosha-chan <nirosha.chanchaiah@themindx.com>
2025-02-13 04:37:08 +0000

    Initial commit

--- file/utils/api.py
+++ file/utils/api.py
@@ -5,5 +5,5 @@ router = APIRouter()
 
 # Lazy import inside a function
 def import_main():
-    from app.chat import get_latest_assistant_response
+    from fastapi_app.main import get_latest_assistant_response
     return get_latest_assistant_response
